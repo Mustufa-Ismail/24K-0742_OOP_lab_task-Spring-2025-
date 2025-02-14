e49d3fff78810f894ac9f4fde36c4b5b14ed7d17
Mustufa-Ismail <mustufaismail7942@gmail.com>
2025-02-14 20:56:39 +0500

    Add files via upload

---
+++ Lab-4/Task-1.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class car{
+    private:
+        string brand;
+        string model;
+        float rentalprice;
+        bool availbility;
+
+    public:
+        car(){
+            brand = "Unknown";
+            model = "Generic";
+            rentalprice = 0.0;
+            availbility = true;
+        }
+
+        void update_car(string brand_n,string model_n,float rentalprice_n){
+            brand = brand_n;
+            model = model_n;
+            rentalprice = rentalprice_n;
+        }
+
+        void rentCar(string brand_n,string model_n){
+            if(availbility == false){
+                cout << "Car is not avalible" << endl;
+                return;
+            }
+            else{
+                availbility = false;
+                cout << "Car has been rented to you " << endl;
+                return;
+            }
+        }
+};
+
+int main(){
+    car c1;
+    car c2;
+    car c3;
+
+    c1.update_car("Toyota","yaris",500.26);
+    c2.update_car("Honda","civix",100.56);
+
+    c1.rentCar("Toyota","yaris");
+    c1.rentCar("Toyota","yaris");
+
+}
